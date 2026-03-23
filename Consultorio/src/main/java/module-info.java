9154f812e0d4ccdbb5f7b378a412e780fb691c2e
Santiago <20253ds099@utez.edu.mx>
2026-03-23 08:45:43 -0600

    Commit autorizado por Eliel Rodriguez por unica ocacion en main

---
+++ Consultorio/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.consultorio {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.consultorio to javafx.fxml;
+    exports org.example.consultorio;
+}
